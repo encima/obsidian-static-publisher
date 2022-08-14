5fb6a0498fad9e2e8fe9046a03fb84cfd79cea22
Chris Gwilliams <chrisg@aiven.io>
2022-08-14 13:14:40 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,81 +1,59 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
+const Minio = require("minio");
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface S3PluginSettings {
+	accessKey: string;
+	secret: string;
+	bucket: string;
+	endpoint: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: S3PluginSettings = {
+	accessKey: '',
+	secret: '',
+	bucket: 'obsidian',
+	endpoint: 's3.eu-central-1.wasabisys.com'
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class S3Plugin extends Plugin {
+	settings: S3PluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		var minioClient = new Minio.Client({
+			endPoint: this.settings.endpoint,
+			useSSL: true,
+			accessKey: this.settings.accessKey,
+			secretKey: this.settings.secret,
+			pathStyle: true,
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
 
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'get-file-frontmatter',
+			name: 'Get File Frontmatter',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+				var file = this.app.workspace.getActiveFile()
+				if (file) {
+					var content = this.app.metadataCache.getFileCache(file);
+					if (content?.frontmatter && content.frontmatter['published'] === true) {
+						this.app.vault.read(file).then(lines => {
+							minioClient.putObject(this.settings.bucket, this.app.workspace.getActiveFile()?.path, lines);
+						})
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+					}
 				}
 			}
 		});
+		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new S3SettingTab(this.app, this));
 	}
 
 	onunload() {
@@ -91,46 +69,57 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class S3SettingTab extends PluginSettingTab {
+	plugin: S3Plugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: S3Plugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'S3 Publisher Settings.' });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('S3 Credentials')
+			.setDesc('Enter your S3 creds here')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your key')
+				.setValue(this.plugin.settings.accessKey)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.accessKey = value;
+					await this.plugin.saveSettings();
+				})).addText(text => text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.secret)
+					.onChange(async (value) => {
+						this.plugin.settings.secret = value;
+						await this.plugin.saveSettings();
+					}));
+
+		new Setting(containerEl)
+			.setName('S3 Bucket')
+			.setDesc('Enter your S3 config here')
+			.addText(text => text
+				.setPlaceholder('Enter your bucket')
+				.setValue(this.plugin.settings.bucket)
+				.onChange(async (value) => {
+					this.plugin.settings.bucket = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('S3 Endpoint')
+			.setDesc('Enter your S3 URL here')
+			.addText(text => text
+				.setPlaceholder('Enter your endpoint')
+				.setValue(this.plugin.settings.endpoint)
+				.onChange(async (value) => {
+					this.plugin.settings.endpoint = value;
 					await this.plugin.saveSettings();
 				}));
 	}
